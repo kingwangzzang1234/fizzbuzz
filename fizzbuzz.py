1da55caab5165c4d212ed9e882308daaeb7b9bd5
ulgoon <ulgoon89@gmail.com>
2021-04-01 15:49:10 +0900

    feat: Complete fizz
    Someone has to complete buzz, fizzbuzz
    
    resolved: #1

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,6 +1,10 @@
 def fizzbuzz(num):
     for i in range(1,num+1):
-        print(i)
+        if i%3==0:
+            print('fizz')
+        else:
+            print(i)
+
 if __name__=='__main__':
     user_num = int(input('Type number: '))
     fizzbuzz(user_num)
