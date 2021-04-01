891e15801cf2fc389d0b3e1e38919ab97bac5c25
ulgoon <ulgoon89@gmail.com>
2021-04-01 15:47:16 +0900

    feat: Add iteration

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,6 +1,6 @@
-def fizzbuzz():
-    pass
-
+def fizzbuzz(num):
+    for i in range(1,num+1):
+        print(i)
 if __name__=='__main__':
-    fizzbuzz()
-    print('it works!')
+    user_num = int(input('Type number: '))
+    fizzbuzz(user_num)
