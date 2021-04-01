e90a229e5fa1e331341a577e9889aafc1f47ca91
ulgoon <ulgoon89@gmail.com>
2021-04-01 15:55:46 +0900

    fix: move input into function
    move line no 9 to no 2

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,10 +1,10 @@
-def fizzbuzz(num):
-    for i in range(1,num+1):
+def fizzbuzz():
+    user_num = int(input('Type number: '))
+    for i in range(1,user_num+1):
         if i%3==0:
             print('fizz')
         else:
             print(i)
 
 if __name__=='__main__':
-    user_num = int(input('Type number: '))
-    fizzbuzz(user_num)
+    fizzbuzz()
