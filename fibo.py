91970261be98537c5271e790d5df09af02a187c5
kingwangzzang1234 <wychoi0408@naver.com>
2024-12-16 02:55:52 +0000

    feat: Binet's formula with commentary

--- fibo.py
+++ fibo.py
@@ -1,7 +1,19 @@
+from math import sqrt
+
+
 def fibo(num):
-    if num < 2:
-        return num
-    else:
-        return fibo(num-1) + fibo(num-2)
+    """
+    fibonacci sequence를 구하는 함수 입니다.
+    """
+    # 점화식 코드
+    #if num < 2:
+    #    return num
+    #else:
+    #    return fibo(num-1) + fibo(num-2)
+
+    phi = (1 + sqrt(5)) / 2
+    phi2 = (1 - sqrt(5)) / 2
+    return round(pow(phi, num) - pow(phi2, num) / sqrt(5))
 
 print(fibo(5))
+
